5a563d705f80bae0d62ad038e1356cd74772366a
Alexsandro Vieira <alexsandrovieira931@gmail.com>
2021-10-29 01:10:08 -0300

    Criando Class Livre

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -3,9 +3,14 @@ package br.com.dio;
 import model.Gato;
 
 public class PrimeiroPrograma {
+    private static Object Livro;
+
     public static void main(String[] args) {
-       Gato gato = new Gato();
+        Gato gato = new Gato();
         System.out.println(gato);
+
+        Livro = new Livro("O problema dos 3 corpos",300);
+        System.out.println(Livro);
 
 
        /* int a = 5;
@@ -14,3 +19,37 @@ public class PrimeiroPrograma {
 
     }
 }
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
+
